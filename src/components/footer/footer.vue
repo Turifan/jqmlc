cc1d79d54c686175aa670c0bb26006de0a39abe1
Turi <fanjizhu@hotmail.com>
2018-08-21 11:37:50 +0800

    add less files

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,14 +1,39 @@
 <!-- 底部导航 -->
 <template lang="html">
+  <div class="footer">
+    <nav class="footerNav">
+      <a href="#">
+        <div class="">
 
+        </div>
+        <div class="">
+
+        </div>
+      </a>
+      <a href="#"></a>
+      <a href="#"></a>
+      <a href="#"></a>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
-  name:'Footer'
+  name: 'Footer'
 }
 </script>
 
 <style lang="less">
+@import '../../style/mixin';
 
+.footerNav {
+    display: flex;
+    flex: 1;
+    width: 100%;
+    background:
+    a {
+        display: block;
+        width: 25%;
+    }
+}
 </style>

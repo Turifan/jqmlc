058b96aad1e42405427c4cbcfdc8695aa09b806a
Turi <fanjizhu@hotmail.com>
2018-08-31 17:33:50 +0800

    修改首页

--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -2,7 +2,33 @@
 <template lang="html">
   <div class="">
     <Header :title="title"/>
-    
+    <div class="userAll">
+      <div class="userAll-tit">
+        总资产（元）
+      </div>
+      <div class="userAll-num">
+        23197.23
+      </div>
+    </div>
+
+    <div class="home-content">
+      <div class="shareActivity">
+        <div class="">
+          分享有礼
+        </div>
+        <div class="">
+          分享好友赢加息机会
+        </div>
+      </div>
+      <div class="currentAmount">
+        <div class="">
+          灵活猫资金
+        </div>
+        <div class="">
+          5638.30元
+        </div>
+      </div>
+    </div>
     <Footer :active="active" :iconClass="iconClass" :iconActive="iconActive"/>
   </div>
 </template>
@@ -37,4 +63,19 @@ export default {
 </script>
 
 <style lang="less">
+@import '../../style/mixin.less';
+@import '../../style/common.less';
+
+.userAll {
+  padding: 49px 0 60px;
+  background: @main-color;
+  text-align: center;
+  color: #fff;
+  .userAll-tit{
+    font-size: 42px;
+  }
+  .userAll-num{
+    font-size: 112px;
+  }
+}
 </style>
